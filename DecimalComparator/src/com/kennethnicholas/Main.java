078d3b40e6222cbae3825f92c895be777feafa83
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-06 09:51:14 -0600

    JavaPrograms

--- DecimalComparator/src/com/kennethnicholas/Main.java
+++ DecimalComparator/src/com/kennethnicholas/Main.java
@@ -20,7 +20,9 @@ package com.kennethnicholas;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo) {
