3279f200d1005609d551d8d0db52892193cf5bcd
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-06 10:44:29 -0600

    JavaPrograms

--- LastDigitChecker/src/com/kennethnicholas/Main.java
+++ LastDigitChecker/src/com/kennethnicholas/Main.java
@@ -23,7 +23,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        System.out.println(hasSameLastDigit(41,22,71));
+        System.out.println(hasSameLastDigit(23,32,42));
+        System.out.println(hasSameLastDigit(9,99,999));
 
     }
 
