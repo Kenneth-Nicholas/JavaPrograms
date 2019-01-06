7b32719b0f0be22e779fe54e72b9f36d67d2e568
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-06 09:48:58 -0600

    JavaPrograms

---
+++ DecimalComparator/src/com/kennethnicholas/Main.java
@@ -0,0 +1,42 @@
+//
+//        DecimalComparator
+//
+//        Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+//
+//        The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places.
+//        Otherwise return false.
+//
+//        EXAMPLES OF INPUT/OUTPUT:
+//
+//        areEqualByThreeDecimalPlaces(-3.1756, -3.175); should return true since numbers are equal up to 3 decimal places.
+//        areEqualByThreeDecimalPlaces(3.175, 3.176); should return false since numbers are not equal up to 3 decimal places
+//        areEqualByThreeDecimalPlaces(3.0, 3.0); should return true since numbers are equal up to 3 decimal places.
+//
+//        NOTE: The areEqualByThreeDecimalPlaces method needs to be defined as public static like we have been doing so far in the course.
+//        NOTE: Do not add a  main method to solution code.
+
+package com.kennethnicholas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo) {
+
+        boolean testCondition;
+
+        int a = (int)(numberOne * 1_000);
+        int b = (int)(numberTwo * 1_000);
+
+        if (a == b) {
+            testCondition = true;
+        } else {
+            testCondition = false;
+        }
+
+        return testCondition;
+
+    }
+}
