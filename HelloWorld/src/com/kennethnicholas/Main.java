473cfc080a27ca3a95196eb3be65f173b9ea919c
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-06 10:04:54 -0600

    JavaPrograms

---
+++ HelloWorld/src/com/kennethnicholas/Main.java
@@ -0,0 +1,19 @@
+package com.kennethnicholas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Kenneth");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
