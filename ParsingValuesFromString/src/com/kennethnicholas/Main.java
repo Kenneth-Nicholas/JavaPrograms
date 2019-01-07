eb2db189942fe38139d56136a5e59b541a847bd8
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-07 07:17:47 -0600

    JavaPrograms

--- ParsingValuesFromString/src/com/kennethnicholas/Main.java
+++ ParsingValuesFromString/src/com/kennethnicholas/Main.java
@@ -3,6 +3,24 @@ package com.kennethnicholas;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        String numberAsString = "2018.125";
+
+        System.out.println("numberAsString = " + numberAsString);
+
+        // int number = Integer.parseInt(numberAsString);
+
+        double number = Double.parseDouble(numberAsString);
+
+        System.out.println("number = " + number);
+
+        numberAsString += 1; // causes numberAsString to = 20181 // causes numberAsString to = 2018.1251
+
+        number += 1; // causes number to = 2019 // causes numberAsString to = 2019.125
+
+        System.out.println("numberAsString = " + numberAsString);
+
+        System.out.println("number = " + number);
+        
     }
 }
