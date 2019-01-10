f69897600ff3c35d9e39196a96df6e2aa7bb9431
Kenneth-Nicholas <nicholas.kenneth.j@gmail.com>
2019-01-10 14:50:14 -0600

    JavaPrograms

--- AllFactors/src/com/kennethnicholas/Main.java
+++ AllFactors/src/com/kennethnicholas/Main.java
@@ -45,15 +45,15 @@ public class Main {
 
     public static void printFactors(int number) {
 
-        if(number < 1) {
+        if (number < 1) {
 
             System.out.println("Invalid Value");
 
         } else {
 
-            for(int i = 1; i <= number; i++) {
+            for (int i = 1; i <= number; i++) {
 
-                if(number % i == 0) {
+                if (number % i == 0) {
 
                     System.out.print(i + " ");
 
